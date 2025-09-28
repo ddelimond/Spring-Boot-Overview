55577f406c9a383f6d0d1441caeb4091e96df472
Darren Delimond <darrendelimond@gmail.com>
2025-09-28 19:08:55 -0400

    =added custom property to application properties file and used / injected it into the website

--- 06-injection-properties-demo/src/main/java/com/darrendelimond/firstSpringApp/rest/FirstRestController.java
+++ 06-injection-properties-demo/src/main/java/com/darrendelimond/firstSpringApp/rest/FirstRestController.java
@@ -1,10 +1,25 @@
 package com.darrendelimond.firstSpringApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+
+    public String getTeamInfo(){
+
+        return  coachName + " plays for "+ teamName;
+    }
+
 
     @GetMapping("/")
     public String sayHello(){
@@ -12,4 +27,11 @@ public class FirstRestController {
 
         return "Hello Gabriel and Brenda";
     }
+
+    @GetMapping("/workout")
+    public String getWorkout(){
+        return "Run a hard 5k";
+    }
+
+
 }
